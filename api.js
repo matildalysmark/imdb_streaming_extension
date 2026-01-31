a42f0a6d9cb6424425a7981a3f7d5b39a7e8ca11
Matilda Lysmark <lysmarkm@chalmers.se>
2026-01-31 22:33:29 +0100

    add imdb rating fetcher

---
+++ api.js
@@ -0,0 +1,30 @@
+const BASE_URL = "https://api.imdbapi.dev";
+const SEARCH_ENDPOINT = `${BASE_URL}/search/titles`;
+
+async function getRating(movieTitle, limit=1) {
+    const fallback = "N/A";
+
+    const params = new URLSearchParams({
+        query: movieTitle,
+        limit: limit
+    });
+
+    try {
+        const response = await fetch(`${SEARCH_ENDPOINT}?${params}`, { 
+            signal: AbortSignal.timeout(5000) 
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const rating = data.titles?.[0]?.rating?.aggregateRating ?? fallback;
+
+        return rating;
+
+    } catch (e) {
+        console.error("API error:", e);
+        return fallback;
+    }
+}
